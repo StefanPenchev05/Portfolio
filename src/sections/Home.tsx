2f070ba42f81eba82a31567c0a76fcef45d161a9
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-08-02 19:24:25 +0300

    chore: Update homepage URL in package.json

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='bg-dark-bg min-h-full'>
+        <div className='bg-dark-bg'>
             
         </div>
     );
