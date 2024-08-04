6975e7f8ad49a5f33cf59eaf92f7f6e640068316
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-08-05 01:47:01 +0300

    feat: Add Animation to Menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -83,65 +83,69 @@ const NavBar: React.FC = () => {
       {isMenuOpen && (
         <>
           <div
-            className="xl:hidden fixed inset-0 bg-black opacity-50 transition-opacity duration-300"
+            className="xl:hidden fixed inset-0 bg-black opacity-50 transition-opacity duration-1000"
             aria-hidden="true"
             onClick={() => setIsMenuOpen(false)}
           ></div>
-          <div className="xl:hidden fixed left-0 top-0 w-96 p-8 h-full bg-[#191b1e] z-50 transform transition-transform duration-300">
-            <Button
-              className="absolute top-8 right-8"
-              onClick={() => setIsMenuOpen(!isMenuOpen)}
-              roundedFull={true}
-            >
-              <span className="block w-6 h-0.5 bg-primary-red transform rotate-45"></span>
-              <span className="block w-6 h-0.5 bg-primary-red transform -rotate-45 -mt-0.5"></span>
-            </Button>
-            <div className="flex flex-col space-y-12 min-h-full">
-              <div className="space-y-6">
-                <div className="space-y-8">
-                  <div className="flex items-center space-x-4">
-                    <Avatar avatar={lightLogo} />
-                    <NameHeader />
-                  </div>
-                  <p className="text-gray-500 text-pretty leading-b1">
-                    Full-Stack Developer from Bulgaria, lives in Luxembourg,
-                    studying Computer Sience
-                  </p>
-                </div>
-                <Divider />
-              </div>
-              <div className="space-y-20">
-                <div className="flex flex-col mb-12 space-y-6 uppercase">
-                  {tittleAndLinks.map((el, index) => (
-                    <NavItem
-                      key={index}
-                      title={el.title}
-                      link={el.link}
-                      isActive={el.title === activeLink}
-                      onClick={() => handleLinkClick(el.title)}
-                      className="text-base"
-                    />
-                  ))}
-                </div>
-                <Divider />
-              </div>
-              <div className="flex flex-col space-y-4 w-full">
-                <p className="uppercase font-medium text-light-text">
-                  Find with me
-                </p>
-
-                <div className="flex space-x-4">
-                  {contactSocialMediaLinks.map((el, index) => (
-                    <IconButton className="w-14 h-14" link={el.link}>
-                      {el.component}
-                    </IconButton>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
         </>
       )}
+      <div
+        className={`xl:hidden fixed left-0 top-0 w-96 p-8 h-full bg-[#191b1e] overflow-y-auto z-50 transform transition-transform duration-700 delay-100 ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <Button
+          className="absolute top-8 right-8"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          roundedFull={true}
+        >
+          <span className="block w-6 h-0.5 bg-primary-red transform rotate-45"></span>
+          <span className="block w-6 h-0.5 bg-primary-red transform -rotate-45 -mt-0.5"></span>
+        </Button>
+        <div className="flex flex-col space-y-12 min-h-full">
+          <div className="space-y-6">
+            <div className="space-y-8">
+              <div className="flex items-center space-x-4">
+                <Avatar avatar={lightLogo} />
+                <NameHeader />
+              </div>
+              <p className="text-gray-500 text-pretty leading-b1">
+                Full-Stack Developer from Bulgaria, lives in Luxembourg,
+                studying Computer Sience
+              </p>
+            </div>
+            <Divider />
+          </div>
+          <div className="space-y-20">
+            <div className="flex flex-col mb-12 space-y-6 uppercase">
+              {tittleAndLinks.map((el, index) => (
+                <NavItem
+                  key={index}
+                  title={el.title}
+                  link={el.link}
+                  isActive={el.title === activeLink}
+                  onClick={() => handleLinkClick(el.title)}
+                  className="text-base"
+                />
+              ))}
+            </div>
+            <Divider />
+          </div>
+          <div className="flex flex-col space-y-4 w-full">
+            <p className="uppercase font-medium text-light-text">
+              Find with me
+            </p>
+
+            <div className="flex space-x-4">
+              {contactSocialMediaLinks.map((el, index) => (
+                <IconButton className="w-14 h-14" link={el.link}>
+                  {el.component}
+                </IconButton>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
